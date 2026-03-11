2511fc855d83cf1e316ea1c3bfe68d8d86bdcbcf
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-10 21:57:04 -0300

    feat: consertando o backend

--- src/main/java/com/fiap/ec/backend_consultas/BackendConsultasApplication.java
+++ src/main/java/com/fiap/ec/backend_consultas/BackendConsultasApplication.java
@@ -1,5 +1,13 @@
-package com.fiap.ec.backend_consultas.repository;
-import com.fiap.ec.backend_consultas.model.Especialidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+package com.fiap.ec.backend_consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConsultasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendConsultasApplication.class, args);
+    }
+
 }
